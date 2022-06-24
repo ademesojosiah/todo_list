67e46543f856eeeaac60f7b3dc4f0b8e107de0f9
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-06-24 16:58:10 +0100

    added js functionality to the add task and delete button

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+const plus = document.querySelector(".plus");
+const dad = document.querySelector(".main_side3");
 
-let newTask = document.querySelector("#new_item").Value;
+deletebtn();
 
-newTask= "come"
-console.log(newTask)
+plus.addEventListener('click',()=>{
+    let newTask = document.createElement("div")
+    newTask.innerHTML = ` <div class="tasks">
+    <span><input type="checkbox" name="Task3" id="">${document.querySelector('#new_item').value}</span>
+    <img class="bin" src="/icons/bin.png" alt=""></div>`;
+    dad.append(newTask);
+    document.querySelector('#new_item').value = ""  ;
+    
+    deletebtn();
+
+})
+
+
+
+
+function deletebtn(){
+let bins = document.querySelectorAll(".bin");
+for(let bin of bins){
+    bin.addEventListener("click",()=>{
+        bin.parentNode.remove();
+    });
+
+    }
+}
+   
