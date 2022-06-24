3437e4796a7d8a945272c5b648ca38a1afa2a336
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-06-25 00:10:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const plus = document.querySelector(".plus");
 const dad = document.querySelector(".main_side3");
+const newItem = document.querySelector('#new_item')
 
 deletebtn();
 
 plus.addEventListener('click',()=>{
     let newTask = document.createElement("div")
     newTask.classList.add("tasks")
+    let val = newItem.value
     newTask.innerHTML = `
-    <span><input type="checkbox" name="Task3" id="">${document.querySelector('#new_item').value}</span>
+    <span><input type="checkbox" name="Task3" id="">${val}</span>
     <img class="bin" src="/icons/bin.png" alt="">`;
     dad.append(newTask);
     document.querySelector('#new_item').value = ""  ;
