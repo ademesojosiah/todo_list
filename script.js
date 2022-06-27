9f1ee7f3cc42b30f78a3a18a8991d0bc1ebc9e98
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-06-27 20:17:01 +0100

    created a copmpleted tab for all completed task

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 const plus = document.querySelector(".plus");
 const dad = document.querySelector(".main_side3");
-const newItem = document.querySelector('#new_item')
+const comDad = document.querySelector(".completed_task");
+const newItem = document.querySelector('#new_item');
+const comBtn = document.querySelector(".completed");
+const toDo = document.querySelector(".todo");
+const TestInput = document.querySelector(".main_side4");
+const checkings = document.querySelectorAll("#checkbox");
 
-deletebtn();
+
 
 plus.addEventListener('click',()=>{
     let newTask = document.createElement("div")
     newTask.classList.add("tasks")
     let val = newItem.value
     newTask.innerHTML = `
-    <span><input type="checkbox" name="Task3" id="">${val}</span>
+    <span><input type="checkbox" name="Task3" id="checkbox" value="${val}">${val}</span>
     <img class="bin" src="/icons/bin.png" alt="">`;
     dad.append(newTask);
     document.querySelector('#new_item').value = ""  ;
-    
-    deletebtn();
 
+    moveToCompleted();
+    deletebtn();
+   
 })
 
 
+
+
+
+deletebtn();
+
+
+
+toDo.addEventListener('click',()=>{
+    dad.style.display = "block"
+    comDad.style.display ="none"
+    TestInput.style.display ="flex"
+})
+
+comBtn.addEventListener('click',()=>{
+    dad.style.display = "none";
+    comDad.style.display ="block";
+    TestInput.style.display ="none"
+
+   
+
+})
 
 
 function deletebtn(){
@@ -29,5 +56,25 @@ for(let bin of bins){
     });
 
     }
+
+    
 }
+
+function moveToCompleted(){
+for(let i = 0; i<checkings.length; i++){  //looping through the checkings array
+    checkings[i].addEventListener("click",()=>{    //add evenlistener to each of them
+        if(checkings[i].checked == true){  // check if each of the them are checked 
+            let checkers = document.createElement("div");
+            checkers.classList.add("tasks");
+            checkers.innerHTML = `
+            <span>${checkings[i].value}</span>
+            <img class="bin" src="/icons/bin.png" alt="">`;
+            comDad.append(checkers);
+
+            deletebtn();
+    }
+});
+    
+}
+};
    
